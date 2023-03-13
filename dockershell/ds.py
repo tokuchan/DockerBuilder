0a5704128351528331cbdaff4d96d2365247bd7c
sspillane <sspillane@maystreet.com>
2023-03-13 12:06:39 -0400

    Install python and python utilities

--- dockershell/ds.py
+++ dockershell/ds.py
@@ -32,6 +32,9 @@ def getRoot():
 
 def createDockerfile( dockerfile_path:Path ):
     '''Create a new Dockerfile at the specified path.'''
+    log= logging.getLogger('cli.createDockerfile')
+    log.info('Creating Dockerfile file')
+    dockerfile_path.unlink(missing_ok=True)
     with dockerfile_path.open('w') as fout:
         fout.write(textwrap.dedent('''            FROM ubuntu:latest AS base
 
@@ -48,25 +51,39 @@ def createDockerfile( dockerfile_path:Path ):
             RUN git clone https://github.com/timothyvanderaerden/add-apt-repository.git /usr/local/share/add-apt-repository
             RUN chmod ugo+rx /usr/local/share/add-apt-repository/add-apt-repository
             RUN ln -s /usr/local/share/add-apt-repository/add-apt-repository /usr/local/bin/add-apt-repository
+
+            FROM base AS package-install
             
             # Install needed packages
             RUN apt-get update
-            RUN apt-get -y install bash
-            RUN apt-get -y install bat
-            RUN apt-get -y install curl
+
+            # Shells
+            #RUN apt-get -y install bash
             RUN apt-get -y install fish
-            RUN apt-get -y install jq
+            #RUN apt-get -y install tcsh
+            #RUN apt-get -y install zsh
+
+            # System support
             RUN apt-get -y install locales
             RUN apt-get -y install man
+            RUN apt-get -y install sshfs
+            RUN apt-get -y install sudo
+            RUN apt-get -y install stow
+            RUN apt-get -y install unzip
+
+            # Programming languages
+            RUN apt-get -y install python3
+
+            # Utilties
+            RUN apt-get -y install bat
+            RUN apt-get -y install curl
+            RUN apt-get -y install exa
+            RUN apt-get -y install jq
             RUN apt-get -y install ripgrep
             RUN apt-get -y install sqlite3
-            RUN apt-get -y install sshfs
-            RUN apt-get -y install stow
-            RUN apt-get -y install sudo
-            RUN apt-get -y install tcsh
-            RUN apt-get -y install unzip
-            RUN apt-get -y install zsh
             RUN apt -y autoremove
+
+            FROM package-install AS user-setup
 
             #RUN ln -s /home/${user}/host/home/${user}/.Xauthority /home/${user}/.Xauthority
             # Set up a user and switch to that user for the remaining commands
@@ -83,11 +100,24 @@ def createDockerfile( dockerfile_path:Path ):
             RUN dpkg-reconfigure locales
             ENV SSH_AUTH_SOCK=${SSH_AUTH_SOCK}
 
+            FROM user-setup AS python-setup
+
+            RUN mkdir -p /usr/local/share/python-pip
+            RUN curl -Lo /usr/local/share/python-pip/get-pip.py https://bootstrap.pypa.io/get-pip.py
+            RUN sudo python3 /usr/local/share/python-pip/get-pip.py
+
+            RUN pip install --upgrade pip rich-cli termsql ipython
+
+            FROM python-setup AS user-shell
+
             WORKDIR /work
             ENTRYPOINT ["/bin/bash"]
         '''))
 
 def createDsrc(dsrc_path:Path):
+    log= logging.getLogger('cli.createDsrc')
+    log.info('Creating ds.rc file')
+    dsrc_path.unlink(missing_ok=True)
     with dsrc_path.open('w') as fout:
         fout.write(textwrap.dedent(f'''bash
         '''))
@@ -143,13 +173,13 @@ def cli(dry_run, verbose, quiet, init, command, dockerfile=None, dsrc=None):
       ds.rc path ........ {dsrc_path} {'[EXISTS]' if dsrc_path.exists() else '[ABSENT]'}
       '''))
 
-    if init and not dockerfile_path.exists():
+    if init:
         if dry_run:
             log.info('Would have created docker file at: {dockerfile_path}')
         else:
             createDockerfile(dockerfile_path)
 
-    if init and not dsrc_path.exists():
+    if init: 
         if dry_run:
             log.info('Would have created dsrc file at: {dsrc_path}')
         else:
