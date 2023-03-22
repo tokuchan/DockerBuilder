4c55b595ecac1e6d380b8f9cae1b225df0f6d3db
sspillane <sspillane@maystreet.com>
2023-03-21 22:11:15 -0400

    Teach ds how to print a simple script
    In this commit, I add the --script-mode option, in order to print the
    docker commands as a simple script. This way, if I have to run DS on a
    machine that lacks even Python, but does support some kind of shell and
    docker, I can use this mode to generate the script I'd need to run.

--- dockershell/ds.py
+++ dockershell/ds.py
@@ -58,131 +58,125 @@ def getDockerfile(root:Path):
     return root / Path("Dockerfile")
 
 
-def createDockerfile(dockerfile_path: Path):
+def createDockerfile(dockerfile_path: Path, script_mode=False):
     """Create a new Dockerfile at the specified path."""
     log = logging.getLogger("cli.createDockerfile")
     log.info("Creating Dockerfile file")
     dockerfile_path.unlink(missing_ok=True)
+    dockerfile_template= textwrap.dedent(
+        """            FROM ubuntu:latest AS base
+
+        ARG USER
+        ENV user=${USER}
+        ARG UID
+        ENV uid=${UID}
+        ARG ROOTDIR
+        ENV rootdir=${ROOTDIR}
+        ARG WORKDIR
+        ENV workdir=${WORKDIR}
+        
+        # Keep apt tools from prompting
+        ARG DEBIAN_FRONTEND=noninteractive
+        ENV TZ=America/Ny
+        
+        # Inflate the system and set up APT.
+        RUN yes | unminimize
+        RUN apt-get -y install dialog apt-utils tzdata git
+        RUN git clone https://github.com/timothyvanderaerden/add-apt-repository.git /usr/local/share/add-apt-repository
+        RUN chmod ugo+rx /usr/local/share/add-apt-repository/add-apt-repository
+        RUN ln -s /usr/local/share/add-apt-repository/add-apt-repository /usr/local/bin/add-apt-repository
+        RUN apt-get -y update
+        RUN apt-get -y install curl
+        RUN apt-get -y install locales
+        RUN apt-get -y install man
+        RUN apt-get -y install python3
+        RUN apt-get -y install sqlite3
+        RUN apt-get -y install sudo
+        RUN apt -y autoremove
+
+        FROM base AS package-install
+
+        # Shells
+        #RUN apt-get -y install bash
+        RUN apt-get -y install fish
+        #RUN apt-get -y install tcsh
+        #RUN apt-get -y install zsh
+
+        # System support
+        #RUN apt-get -y install sshfs
+        #RUN apt-get -y install stow
+        #RUN apt-get -y install unzip
+
+        # Utilties
+        #RUN apt-get -y install bat
+        #RUN apt-get -y install exa
+        #RUN apt-get -y install jq
+        #RUN apt-get -y install ripgrep
+
+        # Clean up
+        RUN apt -y autoremove
+
+        FROM package-install AS user-setup
+
+        # Handy if you want to support X windows apps within the DS environment
+        #RUN ln -s /home/${user}/host/home/${user}/.Xauthority /home/${user}/.Xauthority
+
+        # Set up a user and switch to that user for the remaining commands
+        RUN useradd -u ${uid} -ms /usr/bin/fish ${user}
+        RUN adduser ${user} sudo
+        RUN echo 'ALL            ALL = (ALL) NOPASSWD: ALL' >> /etc/sudoers
+
+        # Set up environment
+        ENV LANGUAGE="en_US.UTF-8"
+        ENV LC_ALL="en_US.UTF-8"
+        ENV LC_CTYPE="en_US.UTF-8"
+        ENV LANG="en_US.UTF-8"
+        RUN locale-gen en_US.UTF-8
+        RUN dpkg-reconfigure locales
+        ENV SSH_AUTH_SOCK=${SSH_AUTH_SOCK}
+
+        FROM user-setup AS python-setup
+
+        RUN mkdir -p /usr/local/share/python-pip
+        RUN curl -Lo /usr/local/share/python-pip/get-pip.py https://bootstrap.pypa.io/get-pip.py
+        RUN python3 /usr/local/share/python-pip/get-pip.py
+
+        RUN pip install --upgrade pip rich-cli termsql ipython
+
+        FROM python-setup AS user-shell
+
+        WORKDIR ${workdir}
+        CMD ["fish"]
+    """
+        )
+        
     with dockerfile_path.open("w") as fout:
-        fout.write(
-            textwrap.dedent(
-                """            FROM ubuntu:latest AS base
-
-            ARG USER
-            ENV user=${USER}
-            ARG UID
-            ENV uid=${UID}
-            ARG ROOTDIR
-            ENV rootdir=${ROOTDIR}
-            ARG WORKDIR
-            ENV workdir=${WORKDIR}
-            
-            # Keep apt tools from prompting
-            ARG DEBIAN_FRONTEND=noninteractive
-            ENV TZ=America/Ny
-            
-            # Inflate the system and set up APT.
-            RUN yes | unminimize
-            RUN apt-get -y install dialog apt-utils tzdata git
-            RUN git clone https://github.com/timothyvanderaerden/add-apt-repository.git /usr/local/share/add-apt-repository
-            RUN chmod ugo+rx /usr/local/share/add-apt-repository/add-apt-repository
-            RUN ln -s /usr/local/share/add-apt-repository/add-apt-repository /usr/local/bin/add-apt-repository
-            RUN apt-get -y update
-            RUN apt-get -y install curl
-            RUN apt-get -y install locales
-            RUN apt-get -y install man
-            RUN apt-get -y install python3
-            RUN apt-get -y install sqlite3
-            RUN apt-get -y install sudo
-            RUN apt -y autoremove
-
-            FROM base AS package-install
-
-            # Shells
-            #RUN apt-get -y install bash
-            RUN apt-get -y install fish
-            #RUN apt-get -y install tcsh
-            #RUN apt-get -y install zsh
-
-            # System support
-            #RUN apt-get -y install sshfs
-            #RUN apt-get -y install stow
-            #RUN apt-get -y install unzip
-
-            # Utilties
-            #RUN apt-get -y install bat
-            #RUN apt-get -y install exa
-            #RUN apt-get -y install jq
-            #RUN apt-get -y install ripgrep
-
-            # Clean up
-            RUN apt -y autoremove
-
-            FROM package-install AS user-setup
-
-            # Handy if you want to support X windows apps within the DS environment
-            #RUN ln -s /home/${user}/host/home/${user}/.Xauthority /home/${user}/.Xauthority
-
-            # Set up a user and switch to that user for the remaining commands
-            RUN useradd -u ${uid} -ms /usr/bin/fish ${user}
-            RUN adduser ${user} sudo
-            RUN echo 'ALL            ALL = (ALL) NOPASSWD: ALL' >> /etc/sudoers
-
-            # Set up environment
-            ENV LANGUAGE="en_US.UTF-8"
-            ENV LC_ALL="en_US.UTF-8"
-            ENV LC_CTYPE="en_US.UTF-8"
-            ENV LANG="en_US.UTF-8"
-            RUN locale-gen en_US.UTF-8
-            RUN dpkg-reconfigure locales
-            ENV SSH_AUTH_SOCK=${SSH_AUTH_SOCK}
-
-            FROM user-setup AS python-setup
-
-            RUN mkdir -p /usr/local/share/python-pip
-            RUN curl -Lo /usr/local/share/python-pip/get-pip.py https://bootstrap.pypa.io/get-pip.py
-            RUN python3 /usr/local/share/python-pip/get-pip.py
-
-            RUN pip install --upgrade pip rich-cli termsql ipython
-
-            FROM python-setup AS user-shell
-
-            WORKDIR ${workdir}
-            CMD ["fish"]
-        """
-            )
-        )
+        fout.write( dockerfile_template)
 
 
-def createDsrc(dsrc_path: Path):
-    log = logging.getLogger("cli.createDsrc")
-    log.info("Creating ds.rc file")
-    dsrc_path.unlink(missing_ok=True)
-    with dsrc_path.open("w") as fout:
-        fout.write(
-            textwrap.dedent(
-                f"""bash
-        """
-            )
-        )
-
-
-def runCommand(cmd: str, quiet: bool = False):
+def runCommand(cmd: str, quiet: bool = False, exec_mode=False, script_mode=False):
     """Call cmd in the shell, logging output."""
     log = logging.getLogger("cli.runCommand")
-    if quiet:
-        with Popen(
-            cmd, stdout=DEVNULL, stderr=DEVNULL, bufsize=1, universal_newlines=True
-        ) as p:
-            pass
+    if script_mode:
+        print('#!/bin/bash')
+        print(' '.join([str(x) for x in cmd]))
     else:
-        with Popen(
-            cmd, stdout=None, stderr=None, bufsize=1, universal_newlines=True
-        ) as p:
-            pass
+        if exec_mode:
+            os.execlp(*cmd)
+        else:
+            if quiet:
+                with Popen(
+                    cmd, stdout=DEVNULL, stderr=DEVNULL, bufsize=1, universal_newlines=True
+                ) as p:
+                    pass
+            else:
+                with Popen(
+                    cmd, stdout=None, stderr=None, bufsize=1, universal_newlines=True
+                ) as p:
+                    pass
 
-    if p.returncode != 0:
-        raise CalledProcessError(p.returncode, p.args)
+            if p.returncode != 0:
+                raise CalledProcessError(p.returncode, p.args)
 
 
 @click.command(context_settings=dict(ignore_unknown_options=True))
@@ -197,6 +191,7 @@ def runCommand(cmd: str, quiet: bool = False):
 @click.option("--dockerfile", help="Specify a dockerfile, otherwise we guess at one.")
 @click.option("--dsrc", help="Specify a ds.rc command file, otherwise we guess at one.")
 @click.option("-w", "--work-directory", help="Specify the directory to work in.")
+@click.option("-s/-S", "--script-mode/--no-script-mode", help="Instead of running docker, print out a shell script.")
 @click.argument("command", nargs=-1)
 def cli(
     dry_run,
@@ -207,6 +202,7 @@ def cli(
     dockerfile=None,
     dsrc=None,
     work_directory=None,
+    script_mode=False,
 ):
     """
     Using Docker, run the given command within a custom build image.
@@ -217,6 +213,11 @@ def cli(
     tracking this file as your project's build environment). Next, ds runs
     Docker, using that Dockerfile, setting a shell as entrypoint and running
     the supplied command.
+
+    If you wish, you can also use DS to generate a simple shell script, which
+    when run will invoke docker and run the shell. This allows you to generate
+    shell scripts for use on deficient systems where the only program you have,
+    other than a shell, is docker itself.
     """
     logging_level = logging.WARN - 10 * verbose + 10 * quiet
     logging.basicConfig(level=logging_level)
@@ -252,7 +253,7 @@ def cli(
         if dry_run:
             log.info("Would have created docker file at: {dockerfile_path}")
         else:
-            createDockerfile(dockerfile_path)
+            createDockerfile(dockerfile_path, script_mode=script_mode)
 
     if dockerfile_path.exists():
         if dry_run:
@@ -279,9 +280,11 @@ def cli(
                     f"WORKDIR={work_directory}"
                 ],
                 quiet=logging_level > logging.INFO,
-            )
+                exec_mode=False,
+                script_mode=script_mode)
             os.chdir(work_directory)
-            os.execlp(
+            runCommand(
+                [
                 "docker",
                 "docker",
                 "run",
@@ -296,5 +299,8 @@ def cli(
                 "-u",
                 user,
                 "dockershell:latest",
-                *command,
-            )
+                *command
+                ],
+                quiet=logging_level > logging.INFO,
+                exec_mode=True,
+                script_mode=script_mode)
